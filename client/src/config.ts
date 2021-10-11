9c464f6ded6754eaad7c76cd84847b7ceb57b3c0
hakimks <hakimks@gmail.com>
2021-10-11 18:06:24 +0300

    New package-lock file

--- client/src/config.ts
+++ client/src/config.ts
@@ -5,7 +5,7 @@ export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com
 export const authConfig = {
   // TODO: Create an Auth0 application and copy values from it into this map. For example:
   // domain: 'dev-nd9990-p4.us.auth0.com',
-  domain: '...',            // Auth0 domain
-  clientId: '...',          // Auth0 client id
+  domain: 'dev-2qmxi8x4.us.auth0.com',            // Auth0 domain
+  clientId: 'jN3Bj6cSXMs6y40LTndN9JN3jfeGxTrKOZEiz4GEHQza0LovtpKacPSBCAzpMo9u',          // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
 }
