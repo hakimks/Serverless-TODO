9d1137b840c5c1bda9b40ef912850e337b95dc3e
hakimks <hakimks@gmail.com>
2021-10-14 08:41:56 +0300

    congigure end points

--- client/src/config.ts
+++ client/src/config.ts
@@ -6,6 +6,6 @@ export const authConfig = {
   // TODO: Create an Auth0 application and copy values from it into this map. For example:
   // domain: 'dev-nd9990-p4.us.auth0.com',
   domain: 'dev-2qmxi8x4.us.auth0.com',            // Auth0 domain
-  clientId: 'jN3Bj6cSXMs6y40LTndN9JN3jfeGxTrKOZEiz4GEHQza0LovtpKacPSBCAzpMo9u',          // Auth0 client id
+  clientId: 'x87Lsecl3t8doQ01MR7X4na8uwy7aaaA',          // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
 }
